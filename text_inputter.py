443f0732fa26b453e1c7d3b59a45b5dc6e3ed0c4
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-26 21:19:34 +0800

    Pseudocode

---
+++ text_inputter.py
@@ -0,0 +1,11 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# This program will ask for the user's input, 
+# then the inputted message will be appended to an assigned text file.
+
+# open a text file
+    # ask for users input
+    # append user's input into the text file
+    # ask user if they want to input another message
+    # if answer is yes, repeat the program
+    # if no, end the program
