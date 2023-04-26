bf08b9dc2676911a17bb3c23e645d5ecd51b56fb
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-26 21:25:14 +0800

    to open text file & asking for user's input

--- text_inputter.py
+++ text_inputter.py
@@ -4,7 +4,9 @@
 # then the inputted message will be appended to an assigned text file.
 
 # open a text file
-    # ask for users input
+with open("mylife.txt", "a") as file_input:
+    # ask for user's message input
+    text_message = str(input("Input a message: "))
     # append user's input into the text file
     # ask user if they want to input another message
     # if answer is yes, repeat the program
