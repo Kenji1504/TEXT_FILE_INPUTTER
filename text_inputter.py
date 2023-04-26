5b4954bee309f8f268d006ad95d65cd94266080e
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-26 21:55:43 +0800

    if invalid input, ask user to try again

--- text_inputter.py
+++ text_inputter.py
@@ -3,28 +3,35 @@
 # This program will ask for the user's input, 
 # then the inputted message will be appended to an assigned text file.
 
+import time
+
 # open a text file
 with open("mylife.txt", "a") as file_input:
     # ask for user's message input
-    text_message = str(input("Input a message: "))
+    text_message = str(input("\nInput a message: "))
     # append user's input into the text file
     file_input.write(text_message + "\n")
 # ask user if they want to input another message
-try_again = str(input("Do you want to input another message into the text file? (yes/no): "))
+try_again = str(input("\nDo you want to input another message into the text file? (yes/no): "))
 try_again = try_again.lower()
 while True:
     # if answer is yes, repeat the program
     if try_again == "yes":
         with open("mylife.txt", "a") as file_input:
-            text_message = str(input("Input a message: "))
+            text_message = str(input("\nInput a message: "))
             file_input.write(text_message + "\n")
-        try_again = str(input("Do you want to input another message into the text file? (yes/no): "))
+        try_again = str(input("\nDo you want to input another message into the text file? (yes/no): "))
         try_again = try_again.lower()
     # if no, end the program
     elif try_again == "no":
         break
+    # if invalid program, ask the user again
+    else:
+        print("\033[1m" + "\nINVALID INPUT\n")
+        time.sleep(1)
+        try_again = str(input("Do you want to input another message into the text file? (yes/no): "))
+        try_again = try_again.lower()
 
-import time
 END_PROGRAM = "END"
 time.sleep(1)
 print ("\n" + "\033[1m" + END_PROGRAM.center(90, "-"))
